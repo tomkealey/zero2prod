8724433419cc47cbf2061dabdd39b3d8376fd890
Tom Kealey <the.tom.kealey@gmail.com>
2024-04-20 22:31:14 +0200

    feat: health check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+//! main.rs
+
+use std::net::TcpListener;
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let address = TcpListener::bind("127.0.0.1:8000")?;
+    run(address)?.await
 }
