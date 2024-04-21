843d442aad050912e64c1e14db46245bf81cdae8
Tom Kealey <the.tom.kealey@gmail.com>
2024-04-21 15:58:32 +0200

    refactor: use configuration.toml

--- src/configuration.rs
+++ src/configuration.rs
@@ -33,10 +33,10 @@ impl DatabaseSettings {
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     // Initialise our configuration reader
     let settings = config::Config::builder()
-        // Add configuration values from a file named 'configuration.yaml'
+        // Add configuration values from a file named 'configuration.toml'
         .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+            "configuration.toml",
+            config::FileFormat::Toml,
         ))
         .build()?;
     // Try to convert configuration value it read into
